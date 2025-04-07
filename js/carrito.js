05850301ad7245b22e76422a7e7da700de9d44cc
José García Torrescusa <josegarcia05@iesamachado.org>
2025-04-07 14:13:37 +0200

    Maquetación del carrito

---
+++ js/carrito.js
@@ -0,0 +1,40 @@
+//Carrito de la compra
+var listadelacompra = [];
+var preciosdelacompra = [];
+
+function boquerones() 
+{
+    var precio = 3.87;
+    var anadirprecio = preciosdelacompra.push(precio);
+    var anadirlistacompra = listadelacompra.push('Kilo de boquerones');
+}
+
+function acedias() {
+    var precio = 12.10;
+    var anadirprecio = preciosdelacompra.push(precio);
+    var anadirlistacompra = listadelacompra.push('Kilo de acedias');
+}
+
+function choco() {
+    var precio = 21.00;
+    var anadirprecio = preciosdelacompra.push(precio);
+    var anadirlistacompra = listadelacompra.push('Kilo de choco');
+}
+
+function puntillitas() {
+    var precio = 22.75;
+    var anadirprecio = preciosdelacompra.push(precio);
+    var anadirlistacompra = listadelacompra.push('Kilo de puntillitas');
+}
+
+function doradas() {
+    var precio = 9.90;
+    var anadirprecio = preciosdelacompra.push(precio);
+    var anadirlistacompra = listadelacompra.push('Kilo de doradas');
+}
+
+function sardinas() {
+    var precio = 15.50;
+    var anadirprecio = preciosdelacompra.push(precio);
+    var anadirlistacompra = listadelacompra.push('Kilo de sardinas');
+}
